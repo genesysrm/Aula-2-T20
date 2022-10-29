d6cfab85356f6da2b7d495aeeff81903c91ac8fa
genesysrm <54962126+genesysrm@users.noreply.github.com>
2022-10-28 22:07:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+// let numero1 = 2;
+// let numero2 = 24;
+// let numero3 = 56;
+// let numero4 = 90;
+// let escolha = "Chá"
+// let cafe = "café"
+// let chá = "chá"
+
+// console.log("mensagem um")
+// console.log("mensagem dois")
+
+    // if (escolha == "café"){
+    //  console.log("Da uma chicara de café")
+    // }else if(escolha === "Chá"){
+    //     console.log("Da uma chicara de chá")
+    // }
+    // else{
+    // console.log(":(")
+    // }
+
+
+
+// let maças = 3;
+// let bananas = 5;
+
+// console.log(`Tenho ${maças} maçãs e ${bananas} bananas`)
+//console.log("Tenho " + apples + "maçãs")
+
+// while(bananas > 0) { //0
+//     console.log("Yum, yum!")
+//     bananas = bananas - 1; // 
+// }
+
+// console.log("Acabaram nossas bananas")
+
+// console.log("vamos a iniciar a contagem")
+
+// for (let count = 0; count <= 20; count++){
+//     console.log(count);
+// }
+
+// console.log("Nova contagem")
+
+// for(let contador = 10; contador >= 1; contador = contador - 2) {
+//     console.log(contador);
+// }
+
+// let myVar = "Ramon"
+// console.log(myVar)
+
+// myVar = "Jose"
+// console.log(myVar)
+
+function sayHello(){
+    console.log("Olá alunos!")
+}
+
+let Pessoa = {
+    nome: "Velchael",
+    sobrenome: "Mendez",
+    // ola: sayHello
+};
+    
+
+let Xicara = {};
+    Xicara.cor = "amarelo";
+    Xicara.material = "ceramica";
+    Xicara.tamanho = "grande";
+    Xicara.dono = Pessoa.nome;
+
+console.log(Xicara.dono)
+// console.log(Pessoa.ola())
+// let nomeGlobal = prompt("Escreva seu nome")
+// let sobrenomeGlobal = prompt("Escreva seu sobrenome")
+
+// function nomeCompleto(nomeBloco,sobrenomeBloco){
+//     let nomeCompleto = `${nomeBloco} ${sobrenomeBloco}`
+//     return nomeCompleto
+// }
+
+// console.log(nomeCompleto(nome,sobrenome))
+
+// let a = 3
+// let b = 4
+// let c = 5
+
+// function myFunction(a){
+//     let b = 14;
+//     let d = 30;
+//     console.log(`a: ${a}, b: ${b}, c: ${c}, d:${d}`)
+// }
+
+// myFunction(a)
+
+// console.log(`a: ${a}, b: ${b}, c: ${c}`)
